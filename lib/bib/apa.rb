df12974074eac938cc3fcaf2d27f7cd22f8bf2bd
alu0100785265 <alu0100785265@ull.edu.es>
2015-12-11 18:44:10 +0000

    finalizando

--- lib/bib/apa.rb
+++ lib/bib/apa.rb
@@ -2,51 +2,40 @@ class Apa
     
     
     attr_accessor :list
-    #En la clase solo vamos a tener como variable de instancia @list
-    #que será una lista enlazada con diferentes referencias bibliograficas
+    
     def initialize(valor)
         @list = Lista.new(valor)
         
     end
     
-        #Los nombres de los autores están invertidos
-        #hay que proporcionar el apellido y las iniciales
-        # de todos los autores de un trabajo en particular
-        #las entradas de la lista de referencias debe estar
-        #en orden alfabetico de acuerdo a los apellidos del
-        #primer autor de cada trabajo
+        
     def getAutores(valor)
-        #Creamos un array auxiliar
+        
             raise ArgumentError unless valor.is_a?(Bibliografia)
             nombres = Array.new()
             for i in 1...valor.autores.length do
-                #Dividimos el nombre en dos
-                nombre, apellido = valor.autores[i].split(/\s+/)
-                #Para mostrarlo seria el apellido y la inicial del nombre
-                outName = "#{apellido} #{nombre[0]}."
-                #incluimos el nombre en el array auxiliar
-                nombres.unshift(outName)
+                
+               nombre, apellido = valor.autores[i].split(/\s+/)
+               outName = "#{apellido} #{nombre[0]}."
+               nombres.unshift(outName)
             end
-            #Hacemos que el array de nombres este ordenado
+            
             valor.autores = nombres.sort
-            #puts(valor.autores)
+            
     end
     
 
     def listaBiblios
-        #Invocamos al metodo getAutores
-        #Para formatear los nombres de cada nodo
-        #Y luego recorremos cada nodo de la lista
-        #mostrando su referencia
-        @list.each do |ref|#Al each de la lista enlazada le pasamos un nodo
-            getAutores(ref)#Luego en el metodo each nos quedamos solo con el valor del nodo
+        
+        @list.each do |ref|
+            getAutores(ref)
             ref.autores.each_with_index do |nombre, i|
                 print(nombre)
                 if (i != (ref.autores.length-1))
-                    print(" & ") #Imprimimos el ampersand
+                    print(" & ") 
                 end #if
-            end #end each_with_index
-           p (ref.getReferencia())
-        end #end @list.each
+            end 
+           p (ref.to_s())
+        end 
     end
 end
